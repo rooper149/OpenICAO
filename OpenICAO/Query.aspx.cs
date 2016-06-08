3d255410d58bf73b5f089392dc94f7c802081e36
Ryan S. Cooper <cooper.ryan@centaurisoftware.co>
2016-06-07 19:03:36 -0500

    Initial commit

---
+++ OpenICAO/Query.aspx.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Net;
+using OpenWeather;
+
+namespace OpenICAO
+{
+    public partial class Query : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                if (Request.QueryString["icao"] != null)
+                {
+                    var url =
+                        $"https://www.aviationweather.gov/adds/dataserver_current/httpparam?dataSource=metars&requestType=retrieve&format=xml&stationString={Request.QueryString["icao"]}&hoursBeforeNow=24";
+                    Response.Write(new WebClient().DownloadString(url));
+                }
+                else if ((Request.QueryString["lat"] != null) && (Request.QueryString["lngt"] != null))
+                {
+
+                    var lat = Convert.ToDouble(Request.QueryString["lat"]);
+                    var lngt = Convert.ToDouble(Request.QueryString["lngt"]);
+                    var station = StationList.Instance.Lookup(lat, lngt);
+
+                    var url =
+                       $"https://www.aviationweather.gov/adds/dataserver_current/httpparam?dataSource=metars&requestType=retrieve&format=xml&stationString={station.ICAO}&hoursBeforeNow=24";
+                    Response.Write(new WebClient().DownloadString(url));
+                }
+                else
+                {
+                    throw new Exception("Invalid parameters");
+                }
+            }
+            catch (Exception ex)
+            {
+                Response.Write(ex.Message);
+            }
+        }
+    }
+}
---
+++ OpenICAO/Search.aspx.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using OpenWeather;
+
+namespace OpenICAO
+{
+    public partial class Search : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                if ((Request.QueryString["lat"] != null) && (Request.QueryString["lngt"] != null))
+                {
+                    var lat = Convert.ToDouble(Request.QueryString["lat"]);
+                    var lngt = Convert.ToDouble(Request.QueryString["lngt"]);
+
+                    var station = StationList.Instance.Lookup(lat, lngt);
+
+                    Response.Write(station.ICAO);
+                }
+                else
+                {
+                    throw new Exception("Invalid parameters");
+                }
+            }
+            catch (Exception ex)
+            {
+                Response.Write(ex.Message);
+            }
+        }
+    }
+}
